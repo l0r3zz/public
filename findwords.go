51eed408031309b18c15059eeee6db7b506b6a7a
Geoff White <geoffw@durganetworks.com>
2016-06-26 14:31:35 -0700

    updates

--- findwords.go
+++ findwords.go
@@ -1,101 +1,104 @@
 package main
 
 import (
-     "fmt"
-    "os"
-    "log"
-    "syscall"
-    "path/filepath"
-    "time" 
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"syscall"
+	"time"
 )
 
-func main() {
-    var f *os.File
-    var err error
-
-    if len(os.Args) == 1 || os.Args[1] == "-h" || os.Args[1] == "--help" {
-        fmt.Printf("usage: %s <file1> \n",filepath.Base(os.Args[0]))
-        os.Exit(1)
-    }
-
-    if f, err = os.Open(os.Args[1]); err != nil {
-        log.Println("failed to open the file: ", err)
-        os.Exit(1)
-    }
-    fmt.Printf("File: %s\n---------\n", os.Args[1])
-
-    var start trienode
-    var words [][]byte = make([][]byte,200000)
-    var startofstring int
-    var j int
-   
-    programstart := time.Now()
-    mappedData := mmapFile(f)
-    for i := range mappedData.d {
-        if mappedData.d[i] == '\n' {
-            words[j] = mappedData.d[startofstring:i]
-            addWord(&start, words[j])
-            //fmt.Printf( "%s\n", words[j])
-            startofstring = i + 1
-            j++
-        }
-    }
-
-	for w := range words {
-		findwords(words[w],words[w])
-	}
-
-    delta := time.Now().Sub(programstart)
-    fmt.Printf("Elapsed Time: %v\n", delta)
-}
 // An mmapData is mmap'ed read-only data from a file.
 type mmapData struct {
-    f *os.File
-    d []byte
+	f *os.File
+	d []byte
+}
+
+func main() {
+	var f *os.File
+	var err error
+
+	if len(os.Args) == 1 || os.Args[1] == "-h" || os.Args[1] == "--help" {
+		fmt.Printf("usage: %s <file1> \n", filepath.Base(os.Args[0]))
+		os.Exit(1)
+	}
+
+	if f, err = os.Open(os.Args[1]); err != nil {
+		log.Println("failed to open the file: ", err)
+		os.Exit(1)
+	}
+	fmt.Printf("File: %s\n---------\n", os.Args[1])
+
+	var start trienode
+	var words [][]byte = make([][]byte, 200000)
+	var startofstring int
+	var j int
+
+	programstart := time.Now()
+	mappedData := mmapFile(f)
+	for i := range mappedData.d {
+		if mappedData.d[i] == '\n' {
+			words[j] = mappedData.d[startofstring:i]
+			addWord(&start, words[j])
+			//fmt.Printf( "%s\n", words[j])
+			startofstring = i + 1
+			j++
+		}
+	}
+
+	for w := range words {
+		findwords(words[w], words[w])
+	}
+
+	delta := time.Now().Sub(programstart)
+	fmt.Printf("Elapsed Time: %v\n", delta)
 }
 
 type trienode struct {
-    words        int
-    prefixes    int
-    edges[26]    *trienode
-}
-var nodes []trienode = make([]trienode,10000000)
-var trieindex int
-func addWord( t *trienode, word []byte) *trienode {
-    if len(word) == 0 {
-        t.words = t.words + 1
-    } else {
-        t.prefixes = t.prefixes +1
-         index  := word[0] - 'a'
-        theRest := word[1:]
-        if t.edges[index] == nil {
-            t.edges[index] = &nodes[trieindex]
-            trieindex++
-        }
-        t.edges[index] = addWord(t.edges[index], theRest)
-    }
-    return t
+	words    int
+	prefixes int
+	edges    [26]*trienode
 }
 
-func findwords( word []byte, tail []byte) [
+var nodes []trienode = make([]trienode, 10000000)
+var trieindex int
+
+func addWord(t *trienode, word []byte) *trienode {
+	if len(word) == 0 {
+		t.words = t.words + 1
+	} else {
+		t.prefixes = t.prefixes + 1
+		index := word[0] - 'a'
+		theRest := word[1:]
+		if t.edges[index] == nil {
+			t.edges[index] = &nodes[trieindex]
+			trieindex++
+		}
+		t.edges[index] = addWord(t.edges[index], theRest)
+	}
+	return t
+}
+
+func findwords(word []byte, tail []byte) {
 }
 
 func mmapFile(f *os.File) mmapData {
-        st, err := f.Stat()
-        if err != nil {
-                log.Fatal(err)
-        }
-        size := st.Size()
-        if int64(int(size+4095)) != size+4095 {
-                log.Fatalf("%s: too large for mmap", f.Name())
-        }
-        n := int(size)
-        if n == 0 {
-                return mmapData{f, nil}
-        }
-        data, err := syscall.Mmap(int(f.Fd()), 0, (n+4095)&^4095, syscall.PROT_READ, syscall.MAP_PRIVATE)
-        if err != nil {
-                log.Fatalf("mmap %s: %v", f.Name(), err)
-        }
-        return mmapData{f, data[:n]}
+	st, err := f.Stat()
+	if err != nil {
+		log.Fatal(err)
+	}
+	size := st.Size()
+	if int64(int(size+4095)) != size+4095 {
+		log.Fatalf("%s: too large for mmap", f.Name())
+	}
+	n := int(size)
+	if n == 0 {
+		return mmapData{f, nil}
+	}
+	data, err := syscall.Mmap(int(f.Fd()), 0, (n+4095)&^4095, syscall.PROT_READ, syscall.MAP_PRIVATE)
+	if err != nil {
+		log.Fatalf("mmap %s: %v", f.Name(), err)
+	}
+	return mmapData{f, data[:n]}
 }
--- first.go
+++ first.go
@@ -1,54 +1,55 @@
 package main
+
 // Simple program that lists a file by mmapping the file and blasting it out
 import (
-   "fmt"
-    "os"
-    "log"
-    "syscall"
-    "path/filepath"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"syscall"
 )
 
 // An mmapData is mmap'ed read-only data from a file.
 type mmapData struct {
-		f *os.File
-		d []byte
+	f *os.File
+	d []byte
 }
 
 func mmapFile(f *os.File) mmapData {
-        st, err := f.Stat()
-        if err != nil {
-                log.Fatal(err)
-        }
-        size := st.Size()
-        if int64(int(size+4095)) != size+4095 {
-                log.Fatalf("%s: too large for mmap", f.Name())
-        }
-        n := int(size)
-        if n == 0 {
-                return mmapData{f, nil}
-        }
-        data, err := syscall.Mmap(int(f.Fd()), 0, (n+4095)&^4095, syscall.PROT_READ, syscall.MAP_SHARED)
-        if err != nil {
-                log.Fatalf("mmap %s: %v", f.Name(), err)
-        }
-        return mmapData{f, data[:n]}
+	st, err := f.Stat()
+	if err != nil {
+		log.Fatal(err)
+	}
+	size := st.Size()
+	if int64(int(size+4095)) != size+4095 {
+		log.Fatal("%s: too large for mmap", f.Name())
+	}
+	n := int(size)
+	if n == 0 {
+		return mmapData{f, nil}
+	}
+	data, err := syscall.Mmap(int(f.Fd()), 0, (n+4095)&^4095,
+		syscall.PROT_READ, syscall.MAP_SHARED)
+	if err != nil {
+		log.Fatal("mmap %s: %v", f.Name(), err)
+	}
+	return mmapData{f, data[:n]}
 }
 
-
-
 func main() {
-    var f *os.File
-    var err error
+	var f *os.File
+	var err error
 
-    if len(os.Args) == 1 || os.Args[1] == "-h" || os.Args[1] == "--help" {
-        fmt.Printf("usage: %s <file1> \n",filepath.Base(os.Args[0]))
-        os.Exit(1)
-    }
+	if len(os.Args) == 1 || os.Args[1] == "-h" || os.Args[1] == "--help" {
+		fmt.Printf("usage: %s <file1> \n", filepath.Base(os.Args[0]))
+		os.Exit(1)
+	}
 
-    if f, err = os.Open(os.Args[1]); err != nil {
-        log.Println("failed to open the file: ", err)
-        os.Exit(1)
+	if f, err = os.Open(os.Args[1]); err != nil {
+		log.Println("failed to open the file: ", err)
+		os.Exit(1)
+	}
 
-    mappedData := mmapFile(f)
-    fmt.Printf("File: %s\n%s", os.Args[1], mappedData.d)
+	mappedData := mmapFile(f)
+	fmt.Printf("File: %s\n%s", os.Args[1], mappedData.d)
 }
