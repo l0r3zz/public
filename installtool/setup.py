875cf301cdf999e8c6c34aea08c474afe7abff8a
Geoff White <geoffw@durganetworks.com>
2018-05-25 22:26:31 -0700

    setup.py working for PyPI

--- installtool/setup.py
+++ installtool/setup.py
@@ -5,10 +5,10 @@ with open("README.md","r") as fh:
     long_description = fh.read()
 setup(
     name = "installtool",
-    version = "0.95",
+    version = "0.96.1.2",
     packages = find_packages(),
-    py_modules = ['mylog'],
-    install_requires = ['concurrent.futures', 'pexpect'],
+    py_modules = ['mylog', 'installtool'],
+    install_requires = [ 'pexpect'],
     python_requires='>=3',
 
     # metadata for upload to PyPI
@@ -28,11 +28,11 @@ setup(
     'Development Status :: 4 - Beta',
 
     # Indicate who your project is intended for
-    'Intended Audience :: DevOps',
+    'Intended Audience :: System Administrators',
     'Topic :: Software Development :: Build Tools',
 
     # Pick your license as you wish (should match "license" above)
-     'License :: OSI Approved :: Apache 2.0',
+     'License :: OSI Approved :: Apache Software License',
 
     # Specify the Python versions you support here. In particular, ensure
     # that you indicate whether you support Python 2, Python 3 or both.
